5d27ab8e9fd596ea5901d1ca59b0554f5bf97fc1
Subham Sai Samal <subhamsaisamalcode@gmail.com>
2025-09-06 14:43:20 +0530

    Fix GitHub Pages Deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Sidebar from './components/Layout/Sidebar';
 import AuthModal from './components/Auth/AuthModal';
 import Home from './pages/Home';
